9f429a91ff9fa1167dfa8da803eb1149a9b3cc10
David <mr.sorensen.david@gmail.com>
2019-11-20 15:08:47 -0500

    Refactor index

--- src/Discord.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-export const message = (message) => {
-  client.login(process.env.DISCORD_TOKEN);
-  client.channels.get('646643147362926592').send(message)
-}
-
