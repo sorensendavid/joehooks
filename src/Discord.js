6ded76bb55d2769960ac09413061b69f445b96c8
David <mr.sorensen.david@gmail.com>
2019-11-20 15:02:14 -0500

    Refactor Discord.js

--- src/Discord.js
+++ src/Discord.js
@@ -2,11 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 export const message = (message) => {
-  client.on('ready', () => {
-    console.log('I am ready!');
-    client.channels.get('646643147362926592').send(message)
-  });
-
   client.login(process.env.DISCORD_TOKEN);
+  client.channels.get('646643147362926592').send(message)
 }
 
