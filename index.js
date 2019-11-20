2cd761bf0b3393b4d4d3a4f49a15fde9053e75bd
David <mr.sorensen.david@gmail.com>
2019-11-20 15:25:57 -0500

    Refactor

--- index.js
+++ index.js
@@ -10,61 +10,61 @@ import { upload } from './src/Bluefang'
 
 client.login(process.env.DISCORD_TOKEN);
 
-const message = (message) => {
-  client.channels.get('646643147362926592').send(message)
-}
 
-message(`Webhook server started. Listening on port ${process.env.HTTP_PORT}.`)
+client.on('ready', () => {
+  const message = (message) => {
+    client.channels.get('646643147362926592').send(message)
+  }
 
-http.createServer((req, res) => {
+  message(`Webhook server started. Listening on port ${process.env.HTTP_PORT}.`)
 
-  let body = ''
-  let valid = false
+  http.createServer((req, res) => {
 
-  req.on('data', chunk => {
-    message("Incoming request. Checking secret.")
-    let sig = "sha1=" + crypto.createHmac('sha1', process.env.GITHUB_SECRET).update(chunk.toString()).digest('hex')
-    if (req.headers['x-hub-signature'] == sig) {
-      message("Secret accepted.")
-      valid = true
-    } else {
-      message("Stopping. Secret failed.")
-    }
-    body += chunk.toString()
-  })
+    let body = ''
+    let valid = false
 
-  req.on('end', () => {
-    if (valid) {
-      message("Grabbing commit info...")
-
-      const github = JSON.parse(body)
-      const logMessage = []
-      logMessage.push(`Deploying ${github.commits.length} commits pushed by ${github.pusher.name}.`)
-
-      for (let i = 0; i < github.commits.length; i++) {
-        let message = `> ${github.commits[i].id.substring(0, 7)} ${github.commits[i].message}`
-        logMessage.push(message)
+    req.on('data', chunk => {
+      message("Incoming request. Checking secret.")
+      let sig = "sha1=" + crypto.createHmac('sha1', process.env.GITHUB_SECRET).update(chunk.toString()).digest('hex')
+      if (req.headers['x-hub-signature'] == sig) {
+        message("Secret accepted.")
+        valid = true
+      } else {
+        message("Stopping. Secret failed.")
       }
+      body += chunk.toString()
+    })
 
-      message(logMessage.join('\n'))
+    req.on('end', () => {
+      if (valid) {
+        message("Grabbing commit info...")
 
-      // Deploy App
-      const deploy = exec(`cd ${process.env.LOCAL_REPO} && git pull`)
+        const github = JSON.parse(body)
+        const logMessage = []
+        logMessage.push(`Deploying ${github.commits.length} commits pushed by ${github.pusher.name}.`)
 
-      deploy.on('exit', (code, signal) => {
-        if (code === 0) {
-          message("Successfully pulled changes from repo.")
-          upload(body)
-        } else {
-          message("Stopping. There was a problem pulling changes from repo.")
+        for (let i = 0; i < github.commits.length; i++) {
+          let message = `> ${github.commits[i].id.substring(0, 7)} ${github.commits[i].message}`
+          logMessage.push(message)
         }
-      })
 
-      valid = false
-    }
-    res.end('ok')
-  })
-}).listen(process.env.HTTP_PORT)
+        message(logMessage.join('\n'))
 
+        // Deploy App
+        const deploy = exec(`cd ${process.env.LOCAL_REPO} && git pull`)
 
+        deploy.on('exit', (code, signal) => {
+          if (code === 0) {
+            message("Successfully pulled changes from repo.")
+            upload(body)
+          } else {
+            message("Stopping. There was a problem pulling changes from repo.")
+          }
+        })
 
+        valid = false
+      }
+      res.end('ok')
+    })
+  }).listen(process.env.HTTP_PORT)
+})
